326dc70bbac4574c18df4bf112d795c9cedc8e0c
chokingwin <2205405321@qq.com>
2018-06-18 18:42:31 +0800

    continue..

--- api/22_platform.js
+++ api/22_platform.js
@@ -1,3 +1,8 @@
 const { sep, delimiter, win32, posix } = require('path');
 
 console.log('sep:', sep);
+console.log('win sep:', win32.sep);
+console.log('posix sep:', posix.sep);
+console.log('PATH:', process.env.PATH);
+console.log('delimiter:', delimiter);
+// console.log('sep:', sep);
